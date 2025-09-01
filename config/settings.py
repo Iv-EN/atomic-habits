65c084b12c9d5ec7d5e603d5d3e4fbbc96648df4
Iv-EN <engenivanov@yandex.ru>
2025-09-01 21:10:30 +0300

    fixed the nginx.conf

--- config/settings.py
+++ config/settings.py
@@ -150,15 +150,13 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 
-CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
     "http://158.160.154.204:80",
     "http://127.0.0.1:80",
     "http://158.160.154.204:8000",
     "http://127.0.0.1:8000",
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://read-and-write.example.com",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
